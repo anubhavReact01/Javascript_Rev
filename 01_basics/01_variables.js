d2f9db8eaf8fad2625a66c1e0dffa3f5d98e3070
anubhavReact01 <anubhavreact01@gmail.com>
2025-07-14 07:58:36 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+console.log('New Member');
+
+ let email = "abc@gmail.com";
+ var ph_num ="9399XXXXXX";
+ let city = "Bhilai"; 
+const nam = "Anubhav";
+
+
+console.log(nam);
+//we can use console.table instead of log to see our data also and it shows us in tabular form
+// Syntax: console.table([var_name, var_name,...])
+console.table([email,ph_num,nam,city])
