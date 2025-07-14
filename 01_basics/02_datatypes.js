8e6a697125159948785244d47da032512577425c
anubhavReact01 <anubhavreact01@gmail.com>
2025-07-14 08:04:13 +0000

    datatpes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let num = 23 // typeof num is number
+const nam  = "Anubhav";  
+let city; // typeof city is undefined
+
+let magic = null // Its a standalone value and typeof null is object.
+console.log(typeof num)
+console.log(typeof nam)
+console.log(typeof city)
+console.log(typeof magic)
+
