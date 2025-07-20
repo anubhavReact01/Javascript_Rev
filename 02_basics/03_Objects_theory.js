e35c29ef830bfda20bb6ad918b013ff50f1427e9
Anubhav <anubhavreact01@gmail.com>
2025-07-20 06:42:02 +0000

    Objects_Theory

---
+++ 02_basics/03_Objects_theory.js
@@ -0,0 +1,32 @@
+// Object is a fundamental data type in JavaScript. It is considered a non-primitive or reference data type, distinguishing it from primitive data types like string, number, boolean, undefined, null, and symbol.
+/*
+Key characteristics of JavaScript objects:
+
+Collection of Key-Value Pairs:
+Objects store data as a collection of properties, where each property consists of a unique key (a string or symbol) and an associated value (which can be of any data type, including other objects, functions, or primitive values).
+Dynamic Nature:
+Objects are dynamic, meaning their properties can be added, modified, or deleted at runtime. 
+Mutability:
+Objects are mutable, meaning their content can be changed after creation.
+Foundation of JavaScript:
+Many other built-in JavaScript constructs, such as arrays, functions, and dates, are essentially specialized types of objects.
+
+
+Objects can be created in several ways, including:
+Object Literal: The most common and concise way, using curly braces {}.
+JavaScript
+
+    const person = {
+      firstName: "John",
+      lastName: "Doe",
+      age: 30
+    };
+    
+Object() Constructor: Using the new Object() syntax.
+JavaScript
+
+    const car = new Object();
+    car.make = "Toyota";
+    car.model = "Camry";
+
+ */
