d0c92c8f4ed6df78287345780edfe8699e4571b8
Anubhav <anubhavreact01@gmail.com>
2025-08-13 08:56:19 +0000

    foreach

---
+++ 05_iterations/04_forEach.js
@@ -0,0 +1,21 @@
+const names = ['Anubhav','Om','Arpit','Aman']
+
+
+ //
+names.forEach( (items)=>{
+    console.log(items);
+    
+})
+ // its is very common ki object of array aaye ab usme se value acces krna to forEach is best for that
+const express = [{
+    Yamha :"MT15",CC : 150
+},{
+    Honda : "CBR",CC: 200
+},{
+    Bajaj : "Pulsar", CC:350
+}]
+
+express.forEach( (val)=>{
+    console.log(val.CC);
+    
+})
